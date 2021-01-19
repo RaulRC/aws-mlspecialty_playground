d009edea972db0d2b5e53a9c61459d9f3b68d197
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2021-01-19 11:20:30 +0100

    [IMP] update streamer

--- StreamingLab/stream_data.py
+++ StreamingLab/stream_data.py
@@ -2,12 +2,25 @@ import requests
 import json
 import random
 import time
+import uuid
+import boto3
+import configparser
+
+config = configparser.ConfigParser()
+config.read('streaming.ini')
+
+client = boto3.client('kinesis', region_name=config['streaming']['region'])
+partition_key = str(uuid.uuid4())
 
 while True:
     try:
         r = requests.get('https://randomuser.me/api/?exc=login')
         data = json.dumps(r.json(), ensure_ascii=False)
-        print(data)
+        client.put_record(
+            StreamName=config['streaming']['name'],
+            Data=data,
+            PartitionKey=partition_key
+        )
         time.sleep(random.uniform(0, 1))
     except json.decoder.JSONDecodeError as je:
         print(je)
