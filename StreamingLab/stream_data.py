948910c7959ddfc355952867e1c49cc452a86d4b
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2021-01-20 21:08:32 +0100

    [IMP] minimal code improvement

--- StreamingLab/stream_data.py
+++ StreamingLab/stream_data.py
@@ -11,16 +11,19 @@ config.read('streaming.ini')
 
 client = boto3.client('kinesis', region_name=config['streaming']['region'])
 partition_key = str(uuid.uuid4())
-
+counter = 0
 while True:
     try:
         r = requests.get('https://randomuser.me/api/?exc=login')
         data = json.dumps(r.json(), ensure_ascii=False)
-        client.put_record(
-            StreamName=config['streaming']['name'],
-            Data=data,
-            PartitionKey=partition_key
-        )
+        counter += 1
+
+        print(counter,
+              client.put_record(
+                  StreamName=config['streaming']['name'],
+                  Data=data,
+                  PartitionKey=partition_key)
+              )
         time.sleep(random.uniform(0, 1))
     except json.decoder.JSONDecodeError as je:
         print(je)
